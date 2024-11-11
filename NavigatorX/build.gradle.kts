73451cb24a96ee96ea297dc5fe914e0eb1092560
ibrahim4Hamdy <ibrahembemo2040@gmail.com>
2024-11-11 15:49:54 +0200

    Change

--- NavigatorX/build.gradle.kts
+++ NavigatorX/build.gradle.kts
@@ -11,7 +11,6 @@ android {
 
     defaultConfig {
         minSdk = 23
-        multiDexEnabled =true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -76,6 +75,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation("com.android.support:multidex:1.0.3")
 
 }
